265617f463fc13422df6dd3e2bf5571c613dc4d0
nextentrepreneur <roy.sujit6@gmail.com>
2017-02-22 12:45:34 +0530

    getting visit counts using session framework

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 
 urlpatterns = [
-	url(r'^$', views.index, name='index')
-	url(r'^books/$', views.BookListView.as_view(), name='books'),
+	url(r'^$', views.index, name='index'),
+	#url(r'^books/$', views.BookListView.as_view(), name='books'),
 ]
