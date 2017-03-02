c675b3bf6efd58baa20bd67100c5b20a066e0484
nextentrepreneur <roy.sujit6@gmail.com>
2017-03-02 20:06:14 +0530

    Added files and changes required for deployment to heroku

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre
+from django.views import generic	# To create class based view function
 # Create your views here.
 def index(request):
 	"""
@@ -18,3 +19,17 @@ def index(request):
 		'index.html',
 		context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
 		)
+
+class BookListView(generic.ListView):
+	model = Book
+	context_object_name = 'my_book_list'
+	queryset = Book.objects.filter(title_icontains='programming')[:5] # Get 5 Books containg the title 'programming'
+	template_name = 'books/my_arbitrary_templatete_name_list.html'
+
+	def get_queryset(self):
+		return Book.objects.filter(title_icontains='programming')[:5] # Get 5 books with title 'programming'
+	
+	def get_context_data(self, **kwargs):
+		context = super(BookListView,self).get_context_data(**kwargs)
+		context['some_data'] = 'This is just some data'
+		return context		
